ad7e87da26e36448fa11ba1b3d781d8018e6b7a0
babaygt <yigit.baba@metu.edu.tr>
2024-11-09 15:45:01 +0100

    Add createUser method to user service

--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -1,8 +1,21 @@
 import { BaseService } from './base-service'
 import { IUser, User } from '../models'
+import bcrypt from 'bcrypt'
 
 export class UserService extends BaseService<IUser> {
 	constructor() {
 		super(User)
+	}
+
+	async createUser(userData: Partial<IUser>): Promise<IUser> {
+		if (await this.exists({ email: userData.email })) {
+			throw new Error('Email already exists')
+		}
+		if (await this.exists({ username: userData.username })) {
+			throw new Error('Username already exists')
+		}
+
+		const hashedPassword = await bcrypt.hash(userData.password!, 10)
+		return this.create({ ...userData, password: hashedPassword })
 	}
 }
