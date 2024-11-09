7005a84095c7f2472665ffd167fd698875871c6c
babaygt <yigit.baba@metu.edu.tr>
2024-11-09 15:33:11 +0100

    Add findAll method to base service

--- backend/src/services/base-service.ts
+++ backend/src/services/base-service.ts
@@ -1,7 +1,14 @@
-import { Model, Document } from 'mongoose'
+import { Model, Document, FilterQuery, QueryOptions } from 'mongoose'
 
 export abstract class BaseService<T extends Document> {
 	constructor(private model: Model<T>) {
 		this.model = model
+	}
+
+	async findAll(
+		filter: FilterQuery<T> = {},
+		options: QueryOptions = {}
+	): Promise<T[]> {
+		return this.model.find(filter, null, options)
 	}
 }
