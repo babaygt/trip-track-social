817e2694ea078efbe4edfa06779132513fc74baa
babaygt <yigit.baba@metu.edu.tr>
2024-11-09 15:36:24 +0100

    Add findById method to base service

--- backend/src/services/base-service.ts
+++ backend/src/services/base-service.ts
@@ -1,4 +1,4 @@
-import { Model, Document, FilterQuery, QueryOptions } from 'mongoose'
+import { Model, Document, FilterQuery, QueryOptions, Types } from 'mongoose'
 
 export abstract class BaseService<T extends Document> {
 	constructor(private model: Model<T>) {
@@ -10,5 +10,12 @@ export abstract class BaseService<T extends Document> {
 		options: QueryOptions = {}
 	): Promise<T[]> {
 		return this.model.find(filter, null, options)
+	}
+
+	async findById(id: string): Promise<T | null> {
+		if (!Types.ObjectId.isValid(id)) {
+			throw new Error('Invalid ID format')
+		}
+		return this.model.findById(id)
 	}
 }
