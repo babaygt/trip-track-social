a1903476467319b984d9c764a29b742dc811edaa
babaygt <yigit.baba@metu.edu.tr>
2024-11-09 13:44:26 +0100

    Add index file for centralized model exports

---
+++ backend/src/models/index.ts
@@ -0,0 +1,17 @@
+// Export interfaces and types
+export { IPoint } from './point'
+export { IUser } from './user'
+export { IRoute, TravelMode, Visibility } from './route'
+export { IMessage } from './message'
+export { IConversation } from './conversation'
+export { IComment } from './comment'
+
+// Export models
+export { User } from './user'
+export { Route } from './route'
+export { Message } from './message'
+export { Conversation } from './conversation'
+
+// Export schemas
+export { PointSchema } from './point'
+export { CommentSchema } from './comment'
