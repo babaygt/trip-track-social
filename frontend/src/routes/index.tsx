91ffaf31a43801e09bcde52c4d3d9bd29d5515e6
babaygt <yigit.baba@metu.edu.tr>
2024-11-14 16:32:36 +0100

    Update routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
 import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 
-// eslint-disable-next-line react-refresh/only-export-components
 const Register = lazy(() => import('@/features/auth/register'))
+const Login = lazy(() => import('@/features/auth/login/'))
 
 export const routes: RouteObject[] = [
 	{
 		path: '/register',
 		element: <Register />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
 	},
 ]
