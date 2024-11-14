94c979268b4e68240f3b154b639c86cfda225f3e
babaygt <yigit.baba@metu.edu.tr>
2024-11-14 16:43:39 +0100

    Update root layout

--- frontend/src/components/layout/root-layout.tsx
+++ frontend/src/components/layout/root-layout.tsx
@@ -1,13 +1,15 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Loader2 } from 'lucide-react'
+import { Navbar } from './navbar'
 
 export function RootLayout() {
 	return (
 		<div className='min-h-screen bg-background'>
+			<Navbar />
 			<Suspense
 				fallback={
-					<div className='flex h-screen items-center justify-center'>
+					<div className='flex h-[calc(100vh-4rem)] items-center justify-center'>
 						<Loader2 className='h-8 w-8 animate-spin text-primary' />
 					</div>
 				}
