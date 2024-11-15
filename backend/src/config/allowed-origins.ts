72eec20a2ccc6974af5669986af87158baa8fee3
babaygt <yigit.baba@metu.edu.tr>
2024-11-16 00:56:09 +0100

    Update allowed origins

--- backend/src/config/allowed-origins.ts
+++ backend/src/config/allowed-origins.ts
@@ -1,1 +1,5 @@
-export const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173']
+export const allowedOrigins = [
+	'http://localhost:8080',
+	'http://localhost:5173',
+	'http://0.0.0.0:8080',
+]
