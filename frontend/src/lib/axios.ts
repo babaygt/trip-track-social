fe9fbd834c6a46b0ceed9fc7c1b67e813ccbec7d
babaygt <yigit.baba@metu.edu.tr>
2024-11-16 00:57:13 +0100

    Update axios settings in frontend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
 
 export const api = axios.create({
 	baseURL: BASE_URL,
 	withCredentials: true,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 })
