d49054f8aa21f236ac770cf34a9cf2ee78600671
babaygt <yigit.baba@metu.edu.tr>
2025-01-11 02:40:19 +0100

    Add react query dev tools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import { routes } from './routes'
 import { RootLayout } from './components/layout/root-layout'
@@ -25,6 +26,7 @@ function App() {
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
 			<Toaster position='top-right' toastOptions={{ duration: 4000 }} />
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	)
 }
